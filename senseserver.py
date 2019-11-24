3c0b6904c7e78fddeed4d25b040da2f5b4230ef6
DEVESHTARASIA <DEVESHTARASIA@users.noreply.github.com>
2019-11-24 18:16:51 +0530

    working correctly
    Running at 192.168.2.5:5000

--- senseserver.py
+++ senseserver.py
@@ -1,4 +1,5 @@
-import flask as fk
+import flask
+from flask import jsonify,request
 import mysql.connector
 mydb = mysql.connector.connect(
         host="localhost",
@@ -6,21 +7,30 @@ mydb = mysql.connector.connect(
         passwd="amsdemo@123",
         database="data_analysis"
         )
-
-app = fk.Flask(__name__)
+sensor_dict = {1:'phval',2:'turbval'}
+app = flask.Flask(__name__)
 @app.route('/',methods=['GET'])
 def get_data():
 	data = []
+	temp_dict = {}
 	mycursor = mydb.cursor()
-	mycursor.execute("SELECT * FROM ph_turb")
+	mycursor.execute("SELECT * FROM ph_turb ORDER BY time DESC")
 	rawdata = mycursor.fetchall()
-	for x in rawdata:
-		data.append({'timestamp':x[0],'phval':x[1],'turbidityval':x[2]})
-    
-	return jsonify({'toapp':data})
+	request_data = request.args.get('sensor_index')
+	for i in range(len(request_data)):
+		temp_dict[sensor_dict[int(request_data[i])]] = rawdata[0][int(request_data[i])] 
+#	for x in rawdata:
+#		data.append({'timestamp':x[0],'phval':x[1],'turbidityval':x[2]})
+	    
+	return jsonify({'toapp':temp_dict})
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run()
+    
+        
+    
+
+
     
         
     
